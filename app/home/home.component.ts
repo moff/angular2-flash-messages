0e2d8a7ded677bd037f1768a8c21766b6784613c
Paul Moff <moff.dev@gmail.com>
2016-10-20 15:24:38 +0700

    cleaned up dependencies

--- app/home/home.component.ts
+++ app/home/home.component.ts
@@ -14,7 +14,7 @@ export class HomeComponent {
     
     go() {
         this._flashMessagesService.grayOut(true);
-        this._flashMessagesService.show('we were at home' + Math.random(), { cssClass: 'alert-success', timeout: 10000 });
+        this._flashMessagesService.show('we were at home' + Math.random(), { cssClass: 'alert-success', timeout: 3000 });
         this.router.navigate(['/about']);
     }
 }
--- systemjs.config.js
+++ systemjs.config.js
@@ -25,7 +25,7 @@
       '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
       // other libraries
       'rxjs':                       'npm:rxjs',
-      'angular2-in-memory-web-api': 'npm:angular2-in-memory-web-api',
+      'angular-in-memory-web-api': 'npm:angular-in-memory-web-api',
     },
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
@@ -40,7 +40,7 @@
       rxjs: {
         defaultExtension: 'js'
       },
-      'angular2-in-memory-web-api': {
+      'angular-in-memory-web-api': {
         main: './index.js',
         defaultExtension: 'js'
       }
