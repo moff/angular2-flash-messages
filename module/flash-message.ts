a6b84d39ffb03aee46c29baa724bfe433cabf397
Benoit Sida <benoit.sida@epitech.eu>
2016-12-02 16:19:41 +0100

    Added FlashMessage sequential id property to differentiate them

--- module/flash-message.ts
+++ module/flash-message.ts
@@ -1,6 +1,9 @@
 import { FlashMessageInterface } from './flash-message.interface';
 
 export class FlashMessage implements FlashMessageInterface {
+    static nextId = 0;
+    
+    id: number = (FlashMessage.nextId++);
     text: string = 'default text';
     cssClass: string = '';
     
