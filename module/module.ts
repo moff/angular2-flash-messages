42926c6b074a6baff05dfbe9316b2cf7bfc57cf2
Bernd Artmüller <berndartmueller@gmail.com>
2017-01-23 19:41:03 +0100

    remove unused imports

--- module/module.ts
+++ module/module.ts
@@ -1,4 +1,4 @@
-import { NgModule, ModuleWithProviders, Optional, SkipSelf } from '@angular/core';
+import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule }            from '@angular/common';
 import { FlashMessagesComponent }    from './flash-messages.component';
 import { FlashMessagesService }    from './flash-messages.service';
